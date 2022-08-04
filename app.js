f30d48648e63ea8c068578e616f7f0a6a3d1672b
martingdinardi <martingdinardi@gmail.com>
2022-08-04 18:48:42 -0300

    testing user countries

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+let geoip = require('geoip-country');
+let os = require("os");
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let networkInterfaces = os.networkInterfaces();
+  let geo = geoip.lookup(networkInterfaces['Wi-Fi'][0].address);
+  if (geo.country === "UY") {
+    res.send(`Hey, this website is not available in your country: ${geo.country}`);
+  } else {
+    res.send("welcome to the website")
+  }
 })
 
 app.listen(port, () => {
