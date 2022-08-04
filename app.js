e331159a008f91cbee72c32a7cb9fba89e1a6404
martingdinardi <martingdinardi@gmail.com>
2022-08-04 19:59:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ let geoip = require('geoip-country');
 let os = require("os");
 
 app.get('/', (req, res) => {
-/*   let networkInterfaces = os.networkInterfaces();
+  let networkInterfaces = os.networkInterfaces();
+
+  /*
   let geo = geoip.lookup(networkInterfaces['Wi-Fi'][0].address);
   if (geo.country === "UY") {
     res.send(`Hey, this website is not available in your country: ${geo.country}`);
@@ -14,7 +16,7 @@ app.get('/', (req, res) => {
   } else {
     res.send("Unknown Country")
   } */
-  res.send("testing this app shit")
+  res.send(`testing this app shit ${networkInterfaces}`)
 })
 
 app.listen(port, () => {
